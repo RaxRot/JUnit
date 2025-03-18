93409bb7adfed56d8a829e9e262a6cf830e9a719
Vlad Bulahov <dasistperfektosss@gmail.com>
2025-03-18 21:01:39 +0000

    Add classes

---
+++ src/main/java/com/raxrot/Factorial.java
@@ -0,0 +1,11 @@
+package com.raxrot;
+
+public class Factorial {
+    public int factorial(int number) {
+        int fact = 1;
+        for (int i = 1; i <= number; i++) {
+            fact *= i;
+        }
+        return fact;
+    }
+}
